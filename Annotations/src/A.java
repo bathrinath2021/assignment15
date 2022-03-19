f7c83dafe635c44f932dda3e54e56f0f233a2b0f
bathrinath2021 <82258050+bathrinath2021@users.noreply.github.com>
2022-03-19 17:52:13 +0530

    Add files via upload
    Done

---
+++ Annotations/src/A.java
@@ -0,0 +1,11 @@
+
+public class A {
+
+	@Deprecated
+	public int a1 = 10;
+	@Deprecated
+	public String myMethod() {
+
+		return "Natjr";
+	}
+}
---
+++ Annotations/src/B.java
@@ -0,0 +1,13 @@
+
+public class B {
+
+	public static void main(String[] args) {
+		
+	A a = new A();
+	
+	System.out.println(a.myMethod());
+	System.out.println(a.a1);
+	
+	}
+	
+}
